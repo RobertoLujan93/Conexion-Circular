019f705f95dbee6340ede6a0c31f0f5a9efe95e5
tomasfank <tomasfank@outlook.com>
2022-05-24 19:47:01 -0300

    probandoform1

--- js/wpp.js
+++ js/wpp.js
@@ -1,35 +1,24 @@
-// levanto el a del link de whatssap 
-
-const a_msj_wsp = document.getElementById('WSP');
-console.log(a_msj_wsp);
-
-//  <a id="WSP" href="">Enviar Mensaje por Whatsapp</a>
+let amsjwsp = document.getElementById("WSP");
+console.log(amsjwsp);
 
 
-
-function mensaje_wsp(){
-  //<input id="nombreform" type="text" name="name" class="form-control" id="name" placeholder="Nombre" value="Nombre" required>
-  let nombre = document.getElementById("nombreform")
-  let mail = document.getElementById("mailform")
-  let motivo = document.getElementById("motivoform")
-  let mensajeform = document.getElementById("mensajeform")
+function mensajewsp(){
+  let nombre = document.getElementById("nombreform");
+  let mail = document.getElementById("mailform");
+  let telefono = document.getElementById("telefonoform");
+  let motivo = document.getElementById("motivoform");
+  let mensajeform = document.getElementById("mensajeform");
 
   console.log(nombre);
 
-  //console.log(nombre.value)
+  let mensajefinal = `Nombre: ${nombre.value}\nEmail: ${mail.value}\nTelefono: ${telefono.value}\nMotivo: ${motivo.value}\nMensaje: ${mensajeform.value}`;
+  
 
-  let mensaje_final
-  mensaje_final ="Hola!,%20Mi%20Nombre%20es:%20" + nombre.value +",%20" 
-  mensaje_final =  mensaje_final + "%20Mi%20Mail%20es:%20" + mail.value
-  mensaje_final = mensaje_final + ",%20El%20Motivo%20de%20mi%20contacto%20es%20" + motivo.value + ",%20"
-  mensaje_final = mensaje_final + "%20mensaje%20:%20" + mensajeform.value
+  let mensaje
+  mensaje = "https://api.whatsapp.com/send/?phone=3743411783&text=" + mensajefinal 
+  amsjwsp.href = mensaje  
+};
 
-  var mensaje
-  mensaje = "https://api.whatsapp.com/send/?phone=3743411783&text=" + mensaje_final 
-  //Esto%20es%20una%20prueba
-  a_msj_wsp.href = mensaje  
-}
-
-a_msj_wsp.addEventListener('click', mensaje_wsp)
+amsjwsp.addEventListener("click", mensajewsp);
 
 
