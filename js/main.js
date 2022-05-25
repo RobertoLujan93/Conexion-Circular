a92b4689f68ae8d0685890e1bb956e19826584f5
tomasfank <tomasfank@outlook.com>
2022-05-25 10:08:56 -0300

    form-funcionadndo-pag-terminada

--- js/main.js
+++ js/main.js
@@ -1,33 +1,8 @@
-// let amsjwsp = document.querySelector("#WSP");
-// console.log(amsjwsp);
-
-
-// function mensajewsp(){
-//   let nombre = document.getElementById("nombreform");
-//   let mail = document.getElementById("mailform");
-//   let telefono = document.getElementById("telefonoform");
-//   let motivo = document.getElementById("motivoform");
-//   let mensajeform = document.getElementById("mensajeform");
-
-//   console.log(nombre);
-
-//   let mensajefinal = `Nombre: ${nombre.value}\nEmail: ${mail.value}\nTelefono: ${telefono.value}\nMotivo: ${motivo.value}\nMensaje: ${mensajeform.value}`;
-  
-
-//   let mensaje
-//   mensaje = "https://wa.me/3743411783?text=" + mensajefinal 
-//   amsjwsp.href = mensaje  
-// };
-
-// amsjwsp.addEventListener("click", ()=>{
-//   mensajewsp()
-// });
 
 const evento = document.getElementById('send')
 const enviarFormulario =() => {
         let nombre = document.getElementById('nombre').value;
         let mail = document.getElementById('mail').value;
-        // let telefono = document.getElementById('telefono').value;
         let asunto = document.getElementById('asunto').value;
         let mensaje = document.getElementById('mensaje').value;
         let numero= 3743411783;
